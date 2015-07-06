b45b8fbcb0a08c9b013915afd81dafe300f7a569
Freek Wielstra <fwielstra@xebia.com>
2015-07-06 16:27:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+compareStrings
+==============
+
+Simple utility to compare two iOS / OSX localizable .strings files. Basically checks if keys defined in file #1 is also defined in file #2 and vice-versa
+
+## Usage:
+
+go run strings.go ~/project/src/en.lproj/Localizable.strings ~/project/src/nl.lproj/Localizable.strings
---
+++ strings.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// usage: compareStrings en.lproj/Localizable.strings nl.lproj/Localizable.strings
+
+func getLines(filePath string) map[string]string {
+	file, _ := os.Open(filePath)
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	result := make(map[string]string)
+	for scanner.Scan() {
+		line := scanner.Text()
+		split := strings.Split(line, " = ")
+		if len(split) == 2 {
+			result[split[0]] = split[1]
+		}
+	}
+
+	return result
+}
+
+func main() {
+	leftFile := os.Args[1]
+	rightFile := os.Args[2]
+
+	fmt.Println("Comparing", leftFile, "with", rightFile)
+	left := getLines(leftFile)
+	right := getLines(rightFile)
+
+	for key, _ := range left {
+		rightValue := right[key]
+		if rightValue == "" {
+			fmt.Println("Key", key, "defined in", os.Args[1], "not found in", os.Args[2])
+		}
+	}
+
+	for key, _ := range right {
+		leftValue := left[key]
+		if leftValue == "" {
+			fmt.Println("Key", key, "defined in", os.Args[2], "not found in", os.Args[1])
+		}
+	}
+
+}
